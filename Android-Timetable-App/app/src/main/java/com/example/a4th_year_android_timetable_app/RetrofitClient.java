c970b2bd8b6cdafd329fe363fc86d918b21cb5fa
Alexis Pechon <arpechon@gmail.com>
2023-05-08 15:55:01 +0100

    10th commit

--- Android-Timetable-App/app/src/main/java/com/example/a4th_year_android_timetable_app/RetrofitClient.java
+++ Android-Timetable-App/app/src/main/java/com/example/a4th_year_android_timetable_app/RetrofitClient.java
@@ -4,11 +4,18 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
+
+    //List of the APIs that are being implemented
     private static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
 
-    //private static final String BASE_URL = "https://galway-bus.apis.ie/";
-
+    //The commented code below are the APIs that were going to implemented into my project
+    //but were left in due to severe technical limitation of my project.
     //private final String GET_GALWAY_BUS_URL = "https://galway-bus.apis.ie/api/groute/404/0";
+
+    //List of the API calls and links being stored inside of a variable
+    //private static final String GALWAY_BUS_URL = "https://galway-bus.apis.ie/";
+    //private final String GET_NATIONAL_TRANSPORT_URL = "https://api.nationaltransport.ie/gtfsr/v2/gtfsr?format=json HTTP/1.1";
+    //private final String GET_TRAIN_URL = "http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML";
 
     //Retrofit instance
     private static Retrofit retrofit = null;
