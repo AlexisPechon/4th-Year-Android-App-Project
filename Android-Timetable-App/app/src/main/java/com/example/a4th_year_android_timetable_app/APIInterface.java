e11cc19c8d1daa68413b4e4e65744d6c769ceed4
Alexis Pechon <arpechon@gmail.com>
2023-05-09 14:43:26 +0100

    12th commit

--- Android-Timetable-App/app/src/main/java/com/example/a4th_year_android_timetable_app/APIInterface.java
+++ Android-Timetable-App/app/src/main/java/com/example/a4th_year_android_timetable_app/APIInterface.java
@@ -6,7 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface APIInterface {
-    //@GET("api/groute/404/0")
+    //@GET("api/routetimetable/404/0")
     @GET("/posts")
     Call<List<Posts>> getPosts();
 }
